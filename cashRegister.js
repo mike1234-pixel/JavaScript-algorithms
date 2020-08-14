74c6745ebd3af2686b54ce11cf15cab876bcdef4
Mike <57681651+mike1234-pixel@users.noreply.github.com>
2020-08-14 16:37:10 +0100

    breakthrough
    now have the correct change in the correct denominations in the change array

--- cashRegister.js
+++ cashRegister.js
@@ -1,9 +1,59 @@
 function checkCashRegister(price, cash, cid) {
-  var change;
-  return change;
+  var changeOwed = cash - price; // 0.5
+  var change = [];
+
+  // create new array translating denomination to number
+  var cidNumberDenoms = cid;
+  cidNumberDenoms[0][0] = 0.01;
+  cidNumberDenoms[1][0] = 0.05;
+  cidNumberDenoms[2][0] = 0.1;
+  cidNumberDenoms[3][0] = 0.25;
+  cidNumberDenoms[4][0] = 1.0;
+  cidNumberDenoms[5][0] = 5.0;
+  cidNumberDenoms[6][0] = 10.0;
+  cidNumberDenoms[7][0] = 20.0;
+  cidNumberDenoms[8][0] = 100.0;
+  // cidNumberDenoms
+  //   [ [ 0.01, 1.01 ],
+  //   [ 0.05, 2.05 ],
+  //   [ 0.1, 3.1 ],
+  //   [ 0.25, 4.25 ],
+  //   [ 1, 90 ],
+  //   [ 5, 55 ],
+  //   [ 10, 20 ],
+  //   [ 20, 60 ],
+  //   [ 100, 100 ] ]
+
+  // iterate forwards through the cash drawer
+  for (var i = cidNumberDenoms.length - 1; i >= 0; i--) {
+    // ok
+    ///////////////////////////////////////////////////////////////////////////
+    var denomination = cidNumberDenoms[i][0]; // vars are ok
+    var cashInSlot = cidNumberDenoms[i][1];
+    console.log(denomination, cashInSlot, changeOwed);
+    ///////////////////////////////////////////////////////////////////////////
+    while (denomination <= changeOwed && cashInSlot >= changeOwed) {
+      changeOwed -= denomination;
+      cashInSlot -= denomination;
+      change.push(denomination);
+      console.log(`CHANGE: ${change}`);
+    }
+  }
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+// cidNumberDenoms[i][0] == = all denominations equal to or less than change owed
+
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
 
 // the cash drawer is put into the function - the func is a cash register
 // the cash register calculates what change should be given, and what coins it should be given in, given what's in the drawer
@@ -14,7 +64,18 @@ checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], [
 // picture the function as a real cash register.
 // 1) the function is the cash register
 // 2) the cash drawer is a parameter along with price and change
-// 3) calculate change owed - this will be price minus cash
-// 4) look through cash drawer from highest to lowest, until we find a denomination which is smaller than change, remove it from cash draw, take this away from change owed and push it into a change to give in cash array. 
-// 5) repeat this process until change owed is zero. 
-// 6) take into account the other two possibilities (easy) with simple conditionals.
+// ✔ calculate change owed - this will be cash minus price
+// 4) look through cash drawer from highest to lowest, until we find a denomination which is smaller than change, remove it from cash draw, take this away from change owed and push it into a change to give in cash array.
+
+// ✔ translate denomination from string to number so it can be compared to changeOwed
+// CONDITION
+// ✔ if changeOwed is equal to or greater than denomination...
+// ACTION
+// decrement changeOwed by cidNumberDenoms[i][0]
+// cidNumberDenoms[i][1] by cidNumberDenoms[i][0](to remove change from drawer)
+// push that value into change array(to give back to customer)
+
+// 6) repeat this process until change owed is zero.
+// 7) take into account the other two possibilities(easy) with simple conditionals.
+
+// tip) console.log / check and record your values every step of the way to avoid silly errors
